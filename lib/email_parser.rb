a5dea676447af2833a30d17627dfe003e1abdf72
Fer Hernandez <fh.1111@gmail.com>
2018-11-26 22:26:04 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,13 @@ class EmailParser
 
   attr_accessor :list_of_emails
 
-
+  def parse (list_of_emails)
+    array_emails = []
+    array_emails = list_of_emails.split(",")
+  end
 
 
 end
+
+
+EmailParser.new("avi@test.com, arel@test.com").parse
