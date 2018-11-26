5bddaf611dcd56a5f0a96648f8a3de57c2e9c781
Fer Hernandez <fh.1111@gmail.com>
2018-11-26 22:27:30 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,6 +7,10 @@ class EmailParser
 
   attr_accessor :list_of_emails
 
+  def initialize(list_of_emails)
+
+  end
+
   def parse (list_of_emails)
     array_emails = []
     array_emails = list_of_emails.split(",")
